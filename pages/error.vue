9e9e1103e32756fe703d84b2442b2919c0b695a8
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-10-17 07:34:34 +0530

    init

---
+++ pages/error.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen w-screen app-bg a-center">
+    <p class="text-gray-200">Error</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script>
+definePageMeta({
+  middleware: [
+    () => {
+      return navigateTo('/home')
+    },
+  ],
+})
+</script>
