2cecb90e4b78a8c0f73e10d1a84fd18ba94c24e7
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-10-17 22:46:49 +0530

    changed color

--- components/ui/button/loading.vue
+++ components/ui/button/loading.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center my-2">
     <button
       type="button"
-      class="inline-flex items-center px-4 py-2 bg-gray-800 border border-gray-500 rounded-md font-semibold text-xs text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 cursor-progress"
+      class="inline-flex items-center px-4 py-2 bg-site hover:bg-site-light border border-gray-800 rounded-md font-semibold text-xs text-gray-300 uppercase tracking-widest shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150 cursor-progress"
     >
       <svg
         class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
--- components/ui/button/secondary.vue
+++ components/ui/button/secondary.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     :type="type || 'button'"
-    class="inline-flex items-center px-4 py-2 bg-gray-800 border border-gray-500 rounded-md font-semibold text-xs text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150"
+    class="inline-flex items-center px-4 py-2 bg-site hover:bg-white border border-gray-800 rounded-md font-semibold text-xs text-gray-300 hover:text-gray-800 uppercase tracking-widest shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150"
   >
     <slot />
   </button>
