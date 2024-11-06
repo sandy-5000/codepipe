176604350b002a845e2dd500ab7758b8cd74f9a6
Sandeep <bsandeep@corp.untd.com>
2024-11-06 10:27:26 +0530

    changed textarea border color

--- components/ui/textarea.vue
+++ components/ui/textarea.vue
@@ -7,7 +7,7 @@
       :required="required || false"
       :id="id"
       :class="{
-        'py-2 text-sm text-input w-full placeholder-app placeholder:text-sm border-over-site bg-site border-2 outline-none text-label px-3 focus:border-app-light focus:ring-app-light rounded-md shadow-sm': true,
+        'py-2 text-sm text-input w-full placeholder-app placeholder:text-sm border-over-site bg-site border-2 outline-none text-label px-3 focus:border-app focus:ring-app rounded-md shadow-sm': true,
         [className]: true,
       }"
       :disabled="disabled || false"
